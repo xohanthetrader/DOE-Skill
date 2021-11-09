c453ab6f3d7280dd31947eacf0e7ffffedfac24c
xohanthetrader <bharatharia06@gmail.com>
2021-11-09 21:51:29 +0000

    Level System part 1

--- Pre-DOE-Skill/Assets/Enemies/FireTrap/TrapManager.cs
+++ Pre-DOE-Skill/Assets/Enemies/FireTrap/TrapManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TrapManager : MonoBehaviour
 {
@@ -9,33 +10,48 @@ public class TrapManager : MonoBehaviour
     public EnemyDamageTypes dmgtype;
     public LayerMask player;
     [SerializeField] private SpriteRenderer Trap;
+    private IPlayerHealthManager playerHealthManager;
+    public UnityEvent AdditionalInstructions;
+    public ParticleSystem smoulder;
+    private bool finished;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Trap.enabled = true;
+            playerHealthManager = other.GetComponent<IPlayerHealthManager>();
             StartCoroutine(BlowUpTime());
+            AdditionalInstructions.Invoke();
         }
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !finished)
         {
-            IPlayerHealthManager player = other.GetComponent<IPlayerHealthManager>();
-            player?.TakeDamage(5 * Time.deltaTime,dmgtype);
+            playerHealthManager?.TakeDamage(10 * Time.deltaTime,dmgtype);
+
         }
     }
+
+
 
     IEnumerator BlowUpTime()
     {
         yield return new WaitForSeconds(5);
-        Trap.color = Color.blue;
+        Trap.color = Color.grey;
+
         yield return new WaitForSeconds(0.2f);
         Collider2D Player = Physics2D.OverlapCircle(transform.position, 1, player);
         if (Player != null)
         {
             Player.GetComponent<IPlayerHealthManager>()?.TakeDamage(50,EnemyDamageTypes.Boom);
         }
+
+
+        var main = smoulder.main;
+        main.startColor = Color.black;
+
+        finished = true;
     }
 }
---
+++ Pre-DOE-Skill/Assets/FirstBarricade.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+public class FirstBarricade : MonoBehaviour
+{
+    public TilemapCollider2D barrier;
+    public UnityEvent Spawnnext;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Spawnnext.Invoke();
+            gameObject.GetComponent<TilemapRenderer>().enabled = false;
+            barrier.enabled = false;
+        }
+    }
+}
