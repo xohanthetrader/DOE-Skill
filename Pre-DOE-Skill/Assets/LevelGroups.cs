3cecc564d380339e8e7f129d2d7ca89f115744e7
xohanthetrader <bharatharia06@gmail.com>
2021-12-20 16:14:48 +0000

    Fixed Play Button
    Yesterdays commit

--- Pre-DOE-Skill/Assets/LevelGroups.cs
+++ Pre-DOE-Skill/Assets/LevelGroups.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BarMan : MonoBehaviour
+enum LevelGroups
 {
-
+    Level_Test,
 }
--- Pre-DOE-Skill/Assets/UI/ButtonManager.cs
+++ Pre-DOE-Skill/Assets/UI/ButtonManager.cs
@@ -10,7 +10,10 @@ public class ButtonManager : MonoBehaviour
     public void Play()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("Scenes/Test");
+        int LVLCNT = LevelGroups.GetNames(typeof(LevelGroups)).Length;
+        int chosenLevelNum = Random.Range(0,LVLCNT);
+        LevelGroups Level = ((LevelGroups[])LevelGroups.GetValues(typeof(LevelGroups)))[chosenLevelNum];
+        SceneManager.LoadScene($"{Level.ToString()}-1");
         foreach (PlayerData player in allPlayers)
         {
             player.Health = player.MAXHealth;
