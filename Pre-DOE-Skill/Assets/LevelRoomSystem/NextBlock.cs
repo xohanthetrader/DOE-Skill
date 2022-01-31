9f02ec78f49b3997373bc89d9e6a222742350403
xohanthetrader <bharatharia06@gmail.com>
2022-01-31 20:30:48 +0000

    Fixed next level bug

--- Pre-DOE-Skill/Assets/LevelRoomSystem/NextBlock.cs
+++ Pre-DOE-Skill/Assets/LevelRoomSystem/NextBlock.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,20 +9,16 @@ public class NextBlock : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D col)
     {
-        string Level = SceneManager.GetActiveScene().name;
-        string lvlblock = Level.Substring(0, Level.Length - 2);
-        bool sameLevel = true;
-        string nextLevelStr = "";
-        while (sameLevel)
+        if (col.CompareTag("Player"))
         {
-            int LVLCNT = LevelGroups.GetNames(typeof(LevelGroups)).Length;
-            int chosenLevelNum = Random.Range(0,LVLCNT);
-            LevelGroups nextLevel = ((LevelGroups[])LevelGroups.GetValues(typeof(LevelGroups)))[chosenLevelNum];
-            nextLevelStr = nextLevel.ToString();
-            sameLevel = nextLevelStr != Level;
+            string Level = SceneManager.GetActiveScene().name;
+            string lvlblock = Level.Substring(0, Level.Length - 2);
+            int OtherLVLCNT = LevelGroups.GetNames(typeof(LevelGroups)).Length - 1;
+            int chosenLevelNum = Random.Range(0,OtherLVLCNT);
+            LevelGroups[] nextLevels = (LevelGroups[])LevelGroups.GetValues(typeof(LevelGroups));
+            string[] nextLevelsStr = nextLevels.Select(x => x.ToString()).Where(x => x != lvlblock).ToArray();
+            string nextLevelStr = nextLevelsStr[chosenLevelNum];
+            SceneManager.LoadScene($"{nextLevelStr}-1");
         }
-        
-        SceneManager.LoadScene($"{nextLevelStr}-1");
-        
     }
 }
